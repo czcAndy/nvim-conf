f33bb03a11e239bd1089b0c914fe88a68375d1b1
Andrei <andrei.cazaceanu@gmail.com>
2024-11-21 22:05:45 +0100

    fix formatting

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,13 +1,13 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = false,
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-		})
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  config = function()
+    require("catppuccin").setup({
+      transparent_background = false,
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+    })
+  end,
 }
