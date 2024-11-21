580d3f80370f7994f80e09e4edf9b44114454d7a
Andrei <andrei.cazaceanu@gmail.com>
2024-11-21 21:58:17 +0100

    adds clangd-formatter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,11 +1,13 @@
-return  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function() 
-      require("catppuccin").setup({
-        transparent_background = true
-      })
-    end
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = false,
+			background = {
+				light = "latte",
+				dark = "mocha",
+			},
+		})
+	end,
 }
-
-
