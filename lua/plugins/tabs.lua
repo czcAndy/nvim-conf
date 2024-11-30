e298db733b09393d2c1e5b6f30d2cc99dbd00af5
Andrei <andrei.cazaceanu@gmail.com>
2024-11-30 18:21:45 +0100

    remap keys for nvimtree and tabs plugin

--- lua/plugins/tabs.lua
+++ lua/plugins/tabs.lua
@@ -1,18 +1,29 @@
 return {
-  'romgrk/barbar.nvim',
+  "romgrk/barbar.nvim",
   dependencies = {
-    'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-    'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    "lewis6991/gitsigns.nvim",   -- OPTIONAL: for git status
+    "nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
   },
 
-  init = function() 
-    vim.g.barbar_auto_setup = false 
-    vim.keymap.set('n', '<C-b>', ':BufferPrevious<CR>')
-    vim.keymap.set('n', '<C-n>', ':BufferNext<CR>')
-    vim.keymap.set('n', '<C-q>', ':BufferClose<CR>')
+  init = function()
+    vim.g.barbar_auto_setup = false
+    vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>")
+    vim.keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>")
+    -- Goto buffer in position...
+    vim.keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>")
+    vim.keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>")
+    vim.keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>")
+    vim.keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>")
+    vim.keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>")
+    vim.keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>")
+    vim.keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>")
+    vim.keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>")
+    vim.keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>")
+    vim.keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>")
   end,
+
   opts = {
     noremap = true,
-    silent = true
-  }
-} 
+    silent = true,
+  },
+}
