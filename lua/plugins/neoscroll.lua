b0a21a2336234ba31fbbe1fe91e2e956bbbf41cb
Andrei <andrei.cazaceanu@gmail.com>
2024-11-30 18:29:16 +0100

    new mappings for neoscroll and telescope

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,29 +1,8 @@
 return {
-	"karb94/neoscroll.nvim",
-	config = function()
-		local neoscroll = require("neoscroll")
+  "karb94/neoscroll.nvim",
+  config = function()
+    local neoscroll = require("neoscroll")
 
-		neoscroll.setup({})
-
-		local keymap = {
-			-- Use the "sine" easing function
-			["<C-u>"] = function()
-				neoscroll.ctrl_u({ duration = 250, easing = "sine" })
-			end,
-			["<C-d>"] = function()
-				neoscroll.ctrl_d({ duration = 250, easing = "sine" })
-			end,
-			-- When no value is passed the `easing` option supplied in `setup()` is used
-			["<C-y>"] = function()
-				neoscroll.scroll(-0.1, { move_cursor = false, duration = 250, easing = "sine" })
-			end,
-			["<C-s>"] = function()
-				neoscroll.scroll(0.1, { move_cursor = false, duration = 250, easing = "sine" })
-			end,
-		}
-		local modes = { "n", "v", "x" }
-		for key, func in pairs(keymap) do
-			vim.keymap.set(modes, key, func)
-		end
-	end,
+    neoscroll.setup({})
+  end,
 }
