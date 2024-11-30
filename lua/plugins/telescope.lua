8bc2cbcf53e7db0299a158ebf106e6d304b7c64f
Andrei <andrei.cazaceanu@gmail.com>
2024-11-30 17:49:20 +0100

    add ripgrep as comment for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.8",
-
+    -- needs ripgrep to be installed 
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
